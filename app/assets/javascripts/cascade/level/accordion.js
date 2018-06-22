bc844e39c733b4f53f481ce4035b7e24556b7d23
cbryant <cbryant@SMC-CBRYANT-IM.local>
2018-06-22 10:07:44 -0700

    add tabbing accessibility for collapsabile widget

--- app/assets/javascripts/cascade/level/accordion.js
+++ app/assets/javascripts/cascade/level/accordion.js
@@ -1,9 +1,15 @@
 $(function () {
-
-	$(".accordion .content").not(".accordion.active .content").css("display", "none");
+  $(".accordion .content").not(".accordion.active .content").css("display", "none");
 
 	$(".accordion .header").click(function () {
 		$(this).parent(".accordion").toggleClass("active").children(".content").slideToggle('fast');
-	});
+  });
 
+  $(".accordion").children(".header").keydown(function(e) {
+    if (e.keyCode === 32 || e.keyCode === 13) {
+      
+      $(this).parent(".accordion").toggleClass("active").children(".content").slideToggle('fast');
+      return false
+    }
+  })
 });
