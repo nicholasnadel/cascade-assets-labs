20c0b281488ffd44daed90e1d1a87baeaf1c4d7a
Joey Hoang <mr.joseph.hoang@gmail.com>
2018-08-03 10:26:27 -0700

    Hotfix, removing ES6 syntax because IE sucks

--- app/assets/javascripts/omni_nav/omni-nav-v2.js
+++ app/assets/javascripts/omni_nav/omni-nav-v2.js
@@ -704,37 +704,29 @@ var OmniNav2 = (function() {
   };
   
   //Slightly modified from https://stackoverflow.com/a/47840891/1274724
-  function focusNextElement(activeElem, reverse) { 
-    //Can't use *reverse = false* because IE sucks and doesn't support default arguments
-    reverse = reverse || false;
-    /*check if an element is defined or use activeElement*/
+  //Main changes: added focusPrevElement function. Removed all ES6 syntax since IE sucks
+  function focusNextElement(activeElem) { 
+    var reverse = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
     activeElem = activeElem instanceof HTMLElement ? activeElem : document.activeElement;
 
-    let queryString = [
-        'a:not([disabled]):not([tabindex="-1"])',
-        'button:not([disabled]):not([tabindex="-1"])',
-        'input:not([disabled]):not([tabindex="-1"])',
-        'select:not([disabled]):not([tabindex="-1"])',
-        '[tabindex]:not([disabled]):not([tabindex="-1"])'
-        /* add custom queries here */
-      ].join(','),
-      queryResult = Array.prototype.filter.call(document.querySelectorAll(queryString), elem => {
-        /*check for visibility while always include the current activeElement*/
-        return elem.offsetWidth > 0 || elem.offsetHeight > 0 || elem === activeElem;
-      }),
-      indexedList = queryResult.slice().filter(elem => {
-        /* filter out all indexes not greater than 0 */
-        return elem.tabIndex == 0 || elem.tabIndex == -1 ? false : true;
-      }).sort((a, b) => {
-        /* sort the array by index from smallest to largest */
-        return a.tabIndex != 0 && b.tabIndex != 0 
-          ? (a.tabIndex < b.tabIndex ? -1 : b.tabIndex < a.tabIndex ? 1 : 0) 
-          : a.tabIndex != 0 ? -1 : b.tabIndex != 0 ? 1 : 0;
-      }),
-      focusable = [].concat(indexedList, queryResult.filter(elem => {
-        /* filter out all indexes above 0 */
-        return elem.tabIndex == 0 || elem.tabIndex == -1 ? true : false;
-      }));
+    var queryString = ['a:not([disabled]):not([tabindex="-1"])', 'button:not([disabled]):not([tabindex="-1"])', 'input:not([disabled]):not([tabindex="-1"])', 'select:not([disabled]):not([tabindex="-1"])', '[tabindex]:not([disabled]):not([tabindex="-1"])'
+    /* add custom queries here */
+    ].join(','),
+        queryResult = Array.prototype.filter.call(document.querySelectorAll(queryString), function (elem) {
+      /*check for visibility while always include the current activeElement*/
+      return elem.offsetWidth > 0 || elem.offsetHeight > 0 || elem === activeElem;
+    }),
+        indexedList = queryResult.slice().filter(function (elem) {
+      /* filter out all indexes not greater than 0 */
+      return elem.tabIndex == 0 || elem.tabIndex == -1 ? false : true;
+    }).sort(function (a, b) {
+      /* sort the array by index from smallest to largest */
+      return a.tabIndex != 0 && b.tabIndex != 0 ? a.tabIndex < b.tabIndex ? -1 : b.tabIndex < a.tabIndex ? 1 : 0 : a.tabIndex != 0 ? -1 : b.tabIndex != 0 ? 1 : 0;
+    }),
+        focusable = [].concat(indexedList, queryResult.filter(function (elem) {
+      /* filter out all indexes above 0 */
+      return elem.tabIndex == 0 || elem.tabIndex == -1 ? true : false;
+    }));
 
     /* if reverse is true return the previous focusable element
        if reverse is false return the next focusable element */
@@ -742,7 +734,7 @@ var OmniNav2 = (function() {
       : (focusable[focusable.indexOf(activeElem) + 1] || focusable[0]);
   }
 
-  function focusPrevElement( activeElem ) {
+  function focusPrevElement(activeElem) {
     return focusNextElement(activeElem, true);
   } 
   
