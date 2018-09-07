1eb1439ce1ec17ed88a1a82363569dd74a295b76
nicholasnadel <nicholasnadel@gmail.com>
2018-09-07 06:37:40 -0700

    image slider fallback for ie

--- app/assets/javascripts/widgets/image_slider.js
+++ app/assets/javascripts/widgets/image_slider.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  console.log('hello');
   $(".widget-slides").lightSlider({
     gallery: false,
     item: 1,
@@ -10,9 +11,29 @@ $(document).ready(function () {
     loop: true,
     keyPress: true,
     pauseOnHover: false,
-    // useCSS: true,
+    useCSS: true,
     onSliderLoad: function () {
       $(".widget-slides").removeClass("cS-hidden");
     }
   });
+
+  // IE Fallback for slide images object-fit 
+  if (document.documentMode || /Edge/.test(navigator.userAgent)) {
+    $('.image-slider-wrapper img').each(function () {
+      var t = $(this),
+        s = 'url(' + t.attr('src') + ')',
+        p = t.parent(),
+        d = $('<div></div>');
+
+      p.append(d);
+      d.css({
+        'height': t.parent().css('height'),
+        'background-size': 'cover',
+        'background-repeat': 'no-repeat',
+        'background-position': '50% 20%',
+        'background-image': s
+      });
+      t.hide();
+    });
+  }
 });
