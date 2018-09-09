3c162d4d6765130cf583a14d5f4658d8f85c1c1e
nicholasnadel <nicholasnadel@gmail.com>
2018-09-08 23:10:55 -0700

    update default masthead style

--- app/assets/javascripts/widgets/sub_brand_masthead.js
+++ app/assets/javascripts/widgets/sub_brand_masthead.js
@@ -26,21 +26,21 @@
 
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function dropdownToggle() {
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
-    if (!event.target.matches('.dropdown-inner')) {
+  if (!event.target.matches('.dropdown-inner')) {
 
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
     }
+  }
 }
