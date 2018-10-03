1214546a3110f09b55b61057cc43fb7dd38d3a77
cbryant <cbryant@SMC-CBRYANT-IM.local>
2018-10-03 09:34:34 -0700

    added panther notification alert

--- app/assets/javascripts/widgets/emergency-alert.js
+++ app/assets/javascripts/widgets/emergency-alert.js
@@ -10,9 +10,16 @@ var EmergencyAlert = (function() {
   var initialize = function() {
     emergencyAlertDiv = $('div.emergency-alert');
     alertMessage = $('div.alert-message').text();
-    // Imposter test feed: https://imposter.chapman.edu/rave.rss
-    raveFeed = 'https://content.getrave.com/rss/chapman/channel2';
+    // Imposter test 
+    // raveFeed = 'https://imposter.chapman.edu/rave.rss';
+    raveFeed = 'http://localhost:3000/rave.rss';
+    // raveFeed = 'https://content.getrave.com/rss/chapman/channel2';
     noEmergencyMessage = "There is currently no emergency"; // current Rave message for no emergency
+
+    raveNotificationFeed = 'http://localhost:3000/rave_notification.rss';
+    // raveNotificationFeed = 'http://content.getrave.com/rss/chapman/channel3';
+    noNotificationMessage = 'There is currently no notification'
+    
 
     // If there is already a message (comes from data def in Cascade),
     // don't override the HTML with Rave feed
@@ -24,16 +31,40 @@ var EmergencyAlert = (function() {
         url: raveFeed,
         type: 'GET',
         success: function(data) {
+          debugger
           $(data).find("item").each(function () {
+            debugger
             var title = $(this).find("title").text();
             // Looks for absence of the "no emergency" message
             isEmergency = title.indexOf(noEmergencyMessage) < 0;
           });
-
+          
           if(isEmergency) {
             displayEmergencyAlert();
             fillRaveFeedData(data);
+            return
           }
+
+          $.ajax({
+            url: raveNotificationFeed,
+            type: 'GET',
+            success: function(data) {
+              debugger
+              $(data).find("item").each(function () {
+                debugger
+                var title = $(this).find("title").text();
+                // Looks for absence of the "no notification" message
+                isNotification = title.indexOf(noNotificationMessage) < 0;
+              });
+              
+              if(isNotification) {
+                $('.emergency-alert').addClass('notification')
+                displayEmergencyAlert();
+                fillRaveFeedData(data);
+                return
+              }
+            }
+          })
         }
       });
     }
