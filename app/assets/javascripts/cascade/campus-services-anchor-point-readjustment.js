63f28fe260045b6b03c466ec3753d2dc23ad7ddd
nicholasnadel <nicholasnadel@gmail.com>
2019-03-02 00:26:43 -0800

    improved

--- app/assets/javascripts/cascade/campus-services-anchor-point-readjustment.js
+++ app/assets/javascripts/cascade/campus-services-anchor-point-readjustment.js
@@ -1,6 +1,22 @@
-$('a').click(function(){    
-  divId = $(this).attr('href');
-   $('html, body').animate({
-    scrollTop: $(divId).offset().top - 65
-  }, 100);
-});
+(function($) {
+  $('a[href*=#]:not([href=#])').click(function() 
+  {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) 
+    {
+      
+      var target = $(this.hash),
+      headerHeight = $("#primary-nav").height() + 5; // Get fixed header height
+            
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+              
+      if (target.length) 
+      {
+        $('html,body').animate({
+          scrollTop: target.offset().top - headerHeight
+        }, 500);
+        return false;
+      }
+    }
+  });
+})(jQuery);
