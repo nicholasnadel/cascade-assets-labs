3ef9c1fe57f0982f0472d213f622607ca717ed06
Nicholas Nadel <nicholasnadel@gmail.com>
2019-03-12 15:31:29 -0700

    preliminary attempt coding around invalid WYSIWG anchor links 😑😑😑

--- app/assets/javascripts/cascade/campus-services-anchor-point-readjustment.js
+++ app/assets/javascripts/cascade/campus-services-anchor-point-readjustment.js
@@ -1,18 +1,52 @@
 $(function () {
-  // Remove spaces in anchor link urls
-  $("a").each(function () {
-    $(this).attr('href', encodeURI($(this).attr("href")));
-    
-    var attr = $(this).attr('name');
-    var eyeDee = $(this).attr('id');
-    if (typeof attr !== typeof undefined && attr !== false) {
-      $(this).attr('name', eyeDee)
-    }
-    
-    
-  });
- 
+  // Remove invalid WYSIWYG spaces in anchor link href
 
+  $(function noSpace() {
+    $("a").each(function () {
+      var attr = $(this).attr('href');
+      if (typeof attr !== typeof undefined && attr !== false) {
+        $(this).attr('href', $(this).attr("href").replace(/\s/g, "-"))
+        $(this).attr('href', $(this).attr("href").replace(/\s/g, "-"))
+      }
+      // Remove invalid WYSIWYG spaces in name attributes
+      var attr = $(this).attr('name');
+      if (typeof attr !== typeof undefined && attr !== false) {
+        $(this).attr('name', $(this).attr("name").replace(/\s/g, "-"))
+        $(this).attr('name', $(this).attr("name").replace(/\//g), '-')
+      }
+    });
+  });
+
+  // Replace forward slashes in attrs with dashes
+  $(function noSlash() {
+    $(".a-z a").each(function () {
+      var attr = $(this).attr('href');
+      if (typeof attr !== typeof undefined && attr !== false) {
+        $(this).attr('href', $(this).attr("href").replace(/\//g, "-"))
+      }
+      var attr = $(this).attr('name');
+      if (typeof attr !== typeof undefined && attr !== false) {
+        $(this).attr('name', $(this).attr("name").replace(/\//g, "-"))
+      }
+    });
+  });
+
+  // Replace periods in attrs with dashes
+  $(function noPeriod() {
+    $(".a-z a").each(function () {
+      var attr = $(this).attr('href');
+      if (typeof attr !== typeof undefined && attr !== false) {
+        $(this).attr('href', $(this).attr("href").replace(/\./g, '+'))
+      }
+      var attr = $(this).attr('name');
+      if (typeof attr !== typeof undefined && attr !== false) {
+        $(this).attr('name', $(this).attr("name").replace(/\./g, '+'))
+      }
+    });
+  });
+
+
+  // Smooth scrolling to target
   $('a[href*=#]:not([href=#])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
       location.hostname == this.hostname) {
