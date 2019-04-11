12aa67cb51be02d96a5b59d35baa9585d3da294a
mandythomas <MandySThomas@hotmail.com>
2019-04-10 17:40:34 -0700

    chg selector in the easing function for anchor links

--- app/assets/javascripts/cascade/campus-services-anchor-point-readjustment.js
+++ app/assets/javascripts/cascade/campus-services-anchor-point-readjustment.js
@@ -3,7 +3,7 @@ $(function () {
   var omniHeight = $('div#primary-nav').height();
 
   // Smooth scrolling to target
-  $('a[href*=#]:not([href=#])').click(function () {
+  $('a[href*=\\#]:not([href=\\#])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
       location.hostname == this.hostname) {
 
