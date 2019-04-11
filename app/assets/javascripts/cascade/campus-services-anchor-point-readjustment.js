1290d7166185817f0dbace81873fd10eb3ce5603
nicholasnadel <nicholasnadel@gmail.com>
2019-04-11 07:26:40 -0700

    checks for wywiwyg class, removes snippet from testing

--- app/assets/javascripts/cascade/campus-services-anchor-point-readjustment.js
+++ app/assets/javascripts/cascade/campus-services-anchor-point-readjustment.js
@@ -1,30 +1,45 @@
 // Accounts for Omninav height when scrolling to anchor links in WYSIWYG https://trello.com/c/ZCjBDv9e
 $(function () {
-  var omniHeight = $('div#primary-nav').height();
-
-  // Smooth scrolling to target
-  $('a[href*=#]:not([href=#])').click(function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-      location.hostname == this.hostname) {
-
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+  if ($('.wysiwyg-widget').length > 0) {
+    // Remove spaces in anchor link urls
+    $('a').each(function (removeSpace) {
+      $(this).attr('href', encodeURI($(this).attr('href')));
+      if (typeof attr !== typeof undefined && attr !== false) {
+        // Element has this attribute
+      }
+    });
+    $('a[href*=\\#]:not([href=\\#])').click(function () {
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+              scrollTop: target.offset().top - 125 //offsets for fixed header
+            },
+            1000
+          );
+          return false;
+        }
+      }
+    });
+    //Executed on page load with URL containing an anchor tag.
+    if ($(location.href.split('#')[1])) {
+      var target = $('#' + location.href.split('#')[1]);
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top - omniHeight //offsets for fixed header
-        }, 1000);
+            scrollTop: target.offset().top - 125 //offset height of header here too.
+          },
+          1000
+        );
         return false;
       }
     }
-  });
-  //Executed on page load with URL containing an anchor tag.
-  if ($(location.href.split("#")[1])) {
-    var target = $('#' + location.href.split("#")[1]);
-    if (target.length) {
-      $('html,body').animate({
-        scrollTop: target.offset().top - omniHeight //offset height of header here too.
-      }, 1000);
-      return false;
-    }
   }
 });
+
+function removeSpace() {
+  $('a').replace(/%20/g, ' ');
+}
