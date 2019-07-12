305649835e8ca710ea45e8a9cb4f85468234f0d0
Nicholas Nadel <nicholasnadel@gmail.com>
2019-07-11 17:05:50 -0700

    add toggle to show or hide minimize button; check for campus bird cookie notice and hide info box if it exsists

--- app/assets/javascripts/widgets/campus_map.js
+++ app/assets/javascripts/widgets/campus_map.js
@@ -1,4 +1,4 @@
-$(function () {
+function toggleCampusMapInfo() {
   $("#campusMapWidget span.minimize").click(function () {
     $('#campusMapWidget .box, #campusMapWidget .box-inner').hide();
     $('#campusMapWidget span.info').show();
@@ -7,4 +7,34 @@ $(function () {
     $('#campusMapWidget .box, #campusMapWidget .box-inner').show();
     $('#campusMapWidget span.info').hide();
   })
+}
+
+function hideCampusMapInfo() {
+  $("#campusMapWidget span.minimize").click()
+}
+
+function showCampusMapInfo() {
+  $("#campusMapWidget span.info").click()
+}
+
+function ifFirstVisit() {
+  // var ls = localStorage.getItem('namespace.vistited');
+  // if (ls == null) {
+  //   console.log("Your first visit");
+  //   hideCampusMapInfo();
+  //   localStorage.setItem('namespace.vistited', 1)
+  // }
+  if (document.cookie.indexOf('_viewedMap') <= 0) {
+    $('#campusMapWidget iframe#map_frame').css('height', '100%');
+    $('#campusMapWidget .box, #campusMapWidget .box-inner').hide();
+    $('#campusMapWidget span.info').show();
+  }
+}
+
+$(function () {
+  if ($('.campus-map').length) {
+    console.log('campus map')
+    ifFirstVisit();
+    toggleCampusMapInfo();
+  }
 });
