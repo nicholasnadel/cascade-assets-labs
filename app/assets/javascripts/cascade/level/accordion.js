26b12267a1c89040860b6d6af5a1fb5e5b8d26b6
nicholasnadel <nicholasnadel@gmail.com>
2019-08-01 21:38:39 -0700

    add semantic html for accessibility

--- app/assets/javascripts/cascade/level/accordion.js
+++ app/assets/javascripts/cascade/level/accordion.js
@@ -1,26 +1,21 @@
 $(function () {
-  $(".accordion .content").not(".accordion.active .content").toggle();
-
+  $(".accordion .content").not(".accordion.collapsed .content").toggle();
   $(".accordion .header").click(function () {
-    $(this).parent(".accordion").toggleClass("active").children(".content").slideToggle('fast');
+    $(this).parent(".accordion").attr('aria-expanded', function (i, attr) {
+      return attr == 'true' ? 'false' : 'true'
+    });
+    $(this).parent(".accordion").toggleClass("collapsed").children(".content").slideToggle('fast');
   });
-
   $(".accordion").children(".header").keydown(function (e) {
     if (e.keyCode === 32 || e.keyCode === 13) {
-
-      $(this).parent(".accordion").toggleClass("active").children(".content").slideToggle('fast');
+      $(this).parent(".accordion").toggleClass("collapsed").children(".content").slideToggle('fast');
       return false
     }
   })
-
-
   $(".collapsibles-widget .toggle-expand-collapse").click(function () {
     $(".toggle-expand-collapse").toggleClass("collapsed");
-    $(".accordion").removeClass('active');
-    $(".accordion").toggleClass("active");
+    $(".accordion").removeClass('collapsed expanded');
+    $(".accordion").toggleClass("collapsed");
     $(".accordion .content").toggle();
-    
-    // $(".toggle-expand-collapse").toggleClass("collapsed");
-    // $(".accordion .content").not(".accordion.active .content").toggle();
   });
 });
