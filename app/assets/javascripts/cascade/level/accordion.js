0c9d2297c72f9427801eb9c786c78cba98a76b68
nicholasnadel <nicholasnadel@gmail.com>
2019-08-01 19:15:58 -0700

    basic toggle functionality

--- app/assets/javascripts/cascade/level/accordion.js
+++ app/assets/javascripts/cascade/level/accordion.js
@@ -1,15 +1,26 @@
 $(function () {
-  $(".accordion .content").not(".accordion.active .content").css("display", "none");
+  $(".accordion .content").not(".accordion.active .content").toggle();
 
-	$(".accordion .header").click(function () {
-		$(this).parent(".accordion").toggleClass("active").children(".content").slideToggle('fast');
+  $(".accordion .header").click(function () {
+    $(this).parent(".accordion").toggleClass("active").children(".content").slideToggle('fast');
   });
 
-  $(".accordion").children(".header").keydown(function(e) {
+  $(".accordion").children(".header").keydown(function (e) {
     if (e.keyCode === 32 || e.keyCode === 13) {
-      
+
       $(this).parent(".accordion").toggleClass("active").children(".content").slideToggle('fast');
       return false
     }
   })
+
+
+  $(".collapsibles-widget .toggle-expand-collapse").click(function () {
+    $(".toggle-expand-collapse").toggleClass("collapsed");
+    $(".accordion").removeClass('active');
+    $(".accordion").toggleClass("active");
+    $(".accordion .content").toggle();
+    
+    // $(".toggle-expand-collapse").toggleClass("collapsed");
+    // $(".accordion .content").not(".accordion.active .content").toggle();
+  });
 });
