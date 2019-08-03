fb6e032c30ea3801eda1788d5edf94dac1c248c4
nicholasnadel <nicholasnadel@gmail.com>
2019-08-02 21:17:21 -0700

    boring but robust

--- app/assets/javascripts/cascade/level/accordion.js
+++ app/assets/javascripts/cascade/level/accordion.js
@@ -1,26 +1,45 @@
 $(function () {
-  $(".accordion .content").not(".accordion.active .content").toggle();
-
   $(".accordion .header").click(function () {
-    $(this).parent(".accordion").toggleClass("active").children(".content").slideToggle('fast');
+    $accordion = $(this);
+    //getting the next element
+    $content = $accordion.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(100, function () {
+      //execute this after slideToggle is done
+      //change text of accordion based on visibility of content div
+      $accordion.text(function () {
+        //change text based on condition
+        // return $content.is(":visible") ? "Collapse" : "Expand";
+      });
+    });
   });
 
-  $(".accordion").children(".header").keydown(function (e) {
-    if (e.keyCode === 32 || e.keyCode === 13) {
 
-      $(this).parent(".accordion").toggleClass("active").children(".content").slideToggle('fast');
-      return false
-    }
-  })
-
-
-  $(".collapsibles-widget .toggle-expand-collapse").click(function () {
-    $(".toggle-expand-collapse").toggleClass("collapsed");
-    $(".accordion").removeClass('active');
+  function expandAll() {
+  $(".collapsibles-widget .toggle-expand-collapse.expand").click(function () {
+    $(".accordion").addClass('active');
     $(".accordion").toggleClass("active");
-    $(".accordion .content").toggle();
-    
-    // $(".toggle-expand-collapse").toggleClass("collapsed");
-    // $(".accordion .content").not(".accordion.active .content").toggle();
+    $(".accordion .content").css('display', 'block');
+    console.log('expanding')
+    $('.collapsibles-widget .toggle-expand-collapse.expand').hide()
+    $('.collapsibles-widget .toggle-expand-collapse.collapse').show()
   });
+}
+
+function collapseAll() {
+  $(".collapsibles-widget .toggle-expand-collapse.collapse").click(function () {
+    $(".accordion").removeClass('active');
+    $(".accordion .content").css('display', 'none');
+    console.log('collapsing')
+    $('.collapsibles-widget .toggle-expand-collapse.collapse').hide()
+    $('.collapsibles-widget .toggle-expand-collapse.expand').show()
+
+    // $('.collapsibles-widget .toggle-expand-collapse.expand').toggleClass('hidden')
+
+  })
+}
+
+$('.collapsibles-widget .toggle-expand-collapse.collapse').hide()
+expandAll();
+collapseAll();
 });
