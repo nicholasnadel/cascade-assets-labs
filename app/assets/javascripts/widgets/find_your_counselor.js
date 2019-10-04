8ce4603760d39f3cf245f31c5542fbf5c5b96ba9
Chris <chrs.bryant@gmail.com>
2019-10-04 13:27:44 -0700

    styling find your counselor widget

--- app/assets/javascripts/widgets/find_your_counselor.js
+++ app/assets/javascripts/widgets/find_your_counselor.js
@@ -1,5 +1,37 @@
 $(document).ready( function() {
   $('#find-your-counselor').on('click', function() {
-    console.log('hello world!!')
+    // debugger
+    $('.find-counselor-intro').addClass('move-out-top inactive-top');
+    $('#go-back').removeClass('hidden');
+    $('.find-counselor-applicant-type').removeClass('hidden inactive-top position-above').addClass('active move-in-top');
+  });
+
+  $('#domestic-student-type').on('click', function() {
+    $('.find-counselor-applicant-type').removeClass('active').addClass('move-out-top inactive-top');
+    $('.find-counselor-intro').removeClass('active')
+    $('.find-counselor-school').removeClass('hidden inactive-top position-above').addClass('active move-in-top');
+  });
+
+  $('#domestic-transfer-student-type').on('click', function() {
+    $('.find-counselor-applicant-type').removeClass('active').addClass('move-out-top inactive-top');
+    $('.find-counselor-intro').removeClass('active')
+    $('.find-counselor-transfer').removeClass('hidden inactive-top position-above').addClass('active move-in-top');
+  });
+
+  $('#international-transfer-student-type').on('click', function() {
+    $('.find-counselor-applicant-type').removeClass('active').addClass('move-out-top inactive-top');
+    $('.find-counselor-intro').removeClass('active')
+    $('.find-counselor-international').removeClass('hidden inactive-top position-above').addClass('active move-in-top');
+  });
+
+  $('#go-back').on('click',  function() {
+    var sectionClasses        = ['find-counselor-intro', 'find-counselor-applicant-type',
+                          '     find-counselor-transfer', 'find-counselor-international', 'find-counselor-school'];
+    var sectionPostion        = ['inactive-top', 'inactive-bottom'];
+    var $currentActiveSection = $('.find-counselor-container').find('.active');
+    var currentSectionClass   = sectionClasses[$currentActiveSection.attr('class').split(' ').some( function(class) { return class } )]
+    var 
+
+    debugger
   });
 });
