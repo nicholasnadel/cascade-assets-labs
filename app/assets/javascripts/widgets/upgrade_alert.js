e197c9001e275832947d58771ee6372ff522c9e6
nicholasnadel <nicholasnadel@gmail.com>
2019-10-04 19:40:35 -0700

    swap old update alert with http://browser-update.org/

--- app/assets/javascripts/widgets/upgrade_alert.js
+++ app/assets/javascripts/widgets/upgrade_alert.js
@@ -1,42 +1,24 @@
-function dismiss() {
-  $('div.upgrade-browser-wrapper > a.dismiss, div.upgrade-browser-wrapper span.dismiss, div.upgrade-browser-wrapper label.upgrade-notice, div.upgrade-browser-wrapper label.label-dismiss, div.upgrade-browser-wrapper div.dismiss').click(function () {
-    $('div.upgrade-browser-wrapper, #upgrade-browser').css('display', 'none')
-  })
-}
+var $buoop = {
+  required: {
+    e: -4,
+    f: -3,
+    o: -3,
+    s: -1,
+    c: -3
+  },
+  insecure: true,
+  test: true,
+  api: 2019.10,
+  url: "https://www.chapman.edu/upgrade-browser.aspx",
+};
 
-function localStorageAvailable() {
-  if (typeof (Storage) !== "undefined") {
-    $('#persistence input').click(function () {
-      if ($("#persistence input").is(':checked')) {
-        if (localStorageAvailable())
-          localStorage.DoNotShowMessageAgain = "true";
-      }
-    })
-    return true;
-
-  } else {
-    return false;
-  }
+function $buo_f() {
+  var e = document.createElement("script");
+  e.src = "//browser-update.org/update.min.js";
+  document.body.appendChild(e);
+};
+try {
+  document.addEventListener("DOMContentLoaded", $buo_f, false)
+} catch (e) {
+  window.attachEvent("onload", $buo_f)
 }
-
-function cacheDismissal() {
-  $('#persistence').click(function () {
-    if ($('#persistence').attr('checked')) {
-      if (localStorageAvailable())
-        localStorage.DoNotShowMessageAgain = "true";
-    }
-  })
-}
-
-function checkPref() {
-  if (localStorageAvailable()) {
-    if (localStorage.DoNotShowMessageAgain && localStorage.DoNotShowMessageAgain === "true") {
-      $('div.upgrade-browser-wrapper, #upgrade-browser').css('display', 'none')
-    }
-  };
-}
-$(function () {
-  checkPref();
-  dismiss();
-  cacheDismissal();
-});
