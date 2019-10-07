beb40e5d2203ca6249c9746ee2e2d4700b5fe1af
Nicholas Nadel <nicholasnadel@gmail.com>
2019-10-07 15:54:31 -0700

    improve google search. need to fix responsiveness due to new utility wrapper

---
+++ app/assets/javascripts/uninav/uninav.js
@@ -0,0 +1,6 @@
+window.onload = customPlaceholder;
+
+function customPlaceholder() {
+  document.getElementById("gsc-i-id1").setAttribute("placeholder", "Search...");
+  document.getElementById("gsc-i-id1").style.opacity = "1";
+}
