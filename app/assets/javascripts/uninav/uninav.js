86f0cff1e8c68fb177f98021ea9e6f40a43ef578
Nicholas Nadel <nicholasnadel@gmail.com>
2019-10-08 15:53:52 -0700

    improve search

--- app/assets/javascripts/uninav/uninav.js
+++ app/assets/javascripts/uninav/uninav.js
@@ -1,6 +1,17 @@
-window.onload = customPlaceholder;
+window.onload = gs__customPlaceholder;
+$(window).load(function () {
+  $('#gsc-i-id1').on('input focus click', function () {
+    gs__blurBg();
+  });
+});
 
-function customPlaceholder() {
+function gs__customPlaceholder() {
   document.getElementById("gsc-i-id1").setAttribute("placeholder", "Search...");
   document.getElementById("gsc-i-id1").style.opacity = "1";
+}
+
+function gs__blurBg() {
+  $('.gsc-completion-container').css('background', 'transparent');
+  $('.gsc-completion-container').css('background-color', 'rgba(255, 255, 255, 0.98)');
+
 }
