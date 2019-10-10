1a4069e75607e4ea7133763e8744027665fe667b
Nicholas Nadel <nicholasnadel@gmail.com>
2019-10-10 11:30:46 -0700

    seems to resolve the 'open by default' issue

--- app/assets/javascripts/cascade/level/accordion.js
+++ app/assets/javascripts/cascade/level/accordion.js
@@ -1,5 +1,5 @@
 $(function () {
-  $(".accordion .content").not(".accordion.active .content").css("display", "none");
+  // $(".accordion .content").not(".accordion.active .content").css("display", "none");
   $(".accordion .header").click(function () {
     $(this).parent(".accordion").toggleClass("active").children(".content").slideToggle('fast');
   });
