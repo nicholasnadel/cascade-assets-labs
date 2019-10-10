564678068cde32a1ecba00d0943efc7f446ddf68
Nicholas Nadel <nicholasnadel@gmail.com>
2019-10-10 15:48:39 -0700

    only display if browser supports grid. Add noscript fallback

--- app/assets/javascripts/widgets/upgrade_alert.js
+++ app/assets/javascripts/widgets/upgrade_alert.js
@@ -1,24 +1,26 @@
-var $buoop = {
-  required: {
-    e: -4,
-    f: -3,
-    o: -3,
-    s: -1,
-    c: -3
-  },
-  insecure: true,
-  test: false,
-  api: 2019.10,
-  url: "https://www.chapman.edu/upgrade-browser.aspx",
-};
+if (!(CSS.supports("display: grid"))) {
+  var $buoop = {
+    required: {
+      e: -8,
+      f: -6,
+      o: -3,
+      s: -1,
+      c: -4
+    },
+    insecure: true,
+    test: false,
+    api: 2019.10,
+    url: "https://www.chapman.edu/upgrade-browser.aspx",
+  };
 
-function $buo_f() {
-  var e = document.createElement("script");
-  e.src = "//browser-update.org/update.min.js";
-  document.body.appendChild(e);
-};
-try {
-  document.addEventListener("DOMContentLoaded", $buo_f, false)
-} catch (e) {
-  window.attachEvent("onload", $buo_f)
+  function $buo_f() {
+    var e = document.createElement("script");
+    e.src = "//browser-update.org/update.min.js";
+    document.body.appendChild(e);
+  };
+  try {
+    document.addEventListener("DOMContentLoaded", $buo_f, false)
+  } catch (e) {
+    window.attachEvent("onload", $buo_f)
+  }
 }
