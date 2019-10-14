cc7a8742799a4d8ba3c0f199205b26ddb365e867
Nicholas Nadel <nicholasnadel@gmail.com>
2019-10-14 14:57:51 -0700

    ugly but working on a per-widget basis (when there are multiple)

--- app/assets/javascripts/cascade/level/accordion.js
+++ app/assets/javascripts/cascade/level/accordion.js
@@ -1,3 +1,12 @@
+
+
+$(function() {
+  $.each($('.collapsibles-widget'), function(ind) {
+    $(this).attr('id', 'accordion-' + parseInt(ind + 1));
+  });
+  
+});
+
 $(function () {
   // $(".accordion .content").not(".accordion.active .content").css("display", "none");
   $(".accordion .header").click(function () {
@@ -12,28 +21,70 @@ $(function () {
 
   function expandAll() {
     $(".collapsibles-widget .toggle-expand-collapse.expand").click(function () {
-      $(".accordion").addClass('active');
-      $(".accordion .content").css('display', 'block');
-      $('.collapsibles-widget .toggle-expand-collapse.expand').hide()
-      $('.collapsibles-widget .toggle-expand-collapse.collapse').show()
+      var parent = $(this).closest('.collapsibles-widget').attr('id');
+      parent = '#' + parent;
+      contentClass = ' .content';
+      console.log(parent); 
+      var content = parent + contentClass;
+      console.log(content);
+      
+      var accordionClass = ' .accordion';
+      var currentAccordion = parent + accordionClass;
+
+      var expandClass = ' .toggle-expand-collapse.expand';
+      var collapseClass = ' .toggle-expand-collapse.collapse';
+      var currentExpand = parent + expandClass;
+      var currentCollapse = parent + collapseClass;
+
+      console.log('currentAccordion' + currentAccordion);
+      
+      // var childContent = $('childContent' + parent)
+      // console.log(childContent);
+      // console.log('childcontent::' + content);
+      // childContent = "'" + childContent + "'";
+      // console.log('childcontent w quotes' + childContent);
+      $(content).fadeIn();
+
+      $(currentAccordion).addClass('active');
+      $(currentExpand).hide()
+      $(currentCollapse).fadeIn()
     });
     // keys 🎹
     $(".collapsibles-widget .toggle-expand-collapse.expand").keydown(function (e) {
       if (e.keyCode === 32 || e.keyCode === 13) {
-        $(".accordion").addClass('active');
-        $(".accordion .content").css('display', 'block');
-        $('.collapsibles-widget .toggle-expand-collapse.expand').hide();
-        $('.collapsibles-widget .toggle-expand-collapse.collapse').show().focus();
+        $(currentAccordion).addClass('active');
+        // $(".accordion .content").css('display', 'block');
+        $(content).fadeIn();
+        $(currentExpand).hide();
+        $(currentCollapse).fadeIn().focus();
       }
     })
   }
 
   function collapseAll() {
     $(".collapsibles-widget .toggle-expand-collapse.collapse").click(function () {
-      $(".accordion").removeClass('active');
-      $(".accordion .content").css('display', 'none');
-      $('.collapsibles-widget .toggle-expand-collapse.expand').show();
-      $('.collapsibles-widget .toggle-expand-collapse.collapse').hide();
+      var parent = $(this).closest('.collapsibles-widget').attr('id');
+      parent = '#' + parent;
+
+      console.log('collapse parent: ' + parent)
+      var contentClass = ' .content';
+      var accordionClass = ' .accordion';
+      var content = parent + contentClass;
+
+      var currentAccordion = parent + accordionClass;
+      console.log('currentAccordion' + currentAccordion);
+      
+      var expandClass = ' .toggle-expand-collapse.expand';
+      var collapseClass = ' .toggle-expand-collapse.collapse';
+      var currentExpand = parent + expandClass;
+      var currentCollapse = parent + collapseClass;
+
+
+      console.log('currentAccordion: ' + currentAccordion);
+      $(currentAccordion).removeClass('active');
+      $(content).css('display', 'none');
+      $(currentExpand).fadeIn();
+      $(currentCollapse).hide();
     })
     // keys 🎹
     $(".collapsibles-widget .toggle-expand-collapse.collapse").keydown(function (e) {
@@ -41,11 +92,12 @@ $(function () {
         $(".accordion").removeClass('active');
         $(".accordion .content").css('display', 'none');
         $('.collapsibles-widget .toggle-expand-collapse.collapse').hide();
-        $('.collapsibles-widget .toggle-expand-collapse.expand').show().focus();
+        $('.collapsibles-widget .toggle-expand-collapse.expand').fadeIn().focus();
       }
     })
   }
   $('.collapsibles-widget .toggle-expand-collapse.collapse').hide();
-  expandAll();
+  expandAll();  
   collapseAll();
 });
+
