55a087f573f9c702383232ecee429d20793098ac
Nicholas Nadel <nicholasnadel@gmail.com>
2019-11-01 14:59:53 -0700

    add mobile drilldown JS to its own file

---
+++ app/assets/javascripts/uninav/uninav-mobile-menu.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  console.log('ma5');
+  ma5menu({
+    menu: '.uninav__include-in-mobile-menu',
+    activeClass: 'active',
+    footer: '.uninav--cta',
+    position: 'left',
+    closeOnBodyClick: true
+  });
+});
