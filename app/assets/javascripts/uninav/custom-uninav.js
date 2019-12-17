8c210656e99128ec9cbe693058eeaa4cd4280fe3
Chris <chrs.bryant@gmail.com>
2019-12-17 14:58:48 -0800

    updated navigation drilldown js and css to move items on and off page

--- app/assets/javascripts/uninav/custom-uninav.js
+++ app/assets/javascripts/uninav/custom-uninav.js
@@ -1,9 +1,37 @@
 $(document).ready( function() {
+  var $rootNav = $('.root-umbrella-nav');
+
   $('#uninav .off-canvas-nav .toggle').on('click', function() {
-    var $rootNav = $('.root-umbrella-nav');
-    debugger
     $(this).siblings('.drilldown-menu').show();
     // $(this).siblings('.drilldown-menu').addClass('show');
-    $('.root-umbrella-nav').css({ transform: "translateX(-40rem)"});
+    // console.log($rootNav.css());
+    // debugger
+    var rootNavTranslateXVal = parseInt(getTranslateXVal($rootNav)) - 400;
+
+    $rootNav.css({ transform: "translateX(" + rootNavTranslateXVal + "px"  });
   });
+
+  $('#uninav .off-canvas-nav .menu-back').on('click', function() {
+    // debugger
+    var $backButton = $(this);
+
+    setTimeout(function() {
+      $backButton.parent().hide();
+    }, 1000);
+    
+    var rootNavTranslateXVal = parseInt(getTranslateXVal($rootNav)) + 400;
+    
+    $rootNav.css({ transform: "translateX(" + rootNavTranslateXVal + "px"  });
+  })
+
+  function getTranslateXVal(selector) {
+    var transformMatrix = selector.css("-webkit-transform") ||
+                          selector.css("-moz-transform")    ||
+                          selector.css("-ms-transform")     ||
+                          selector.css("-o-transform")      ||
+                          selector.css("transform");
+    var matrix = transformMatrix.replace(/[^0-9\-.,]/g, '').split(',');
+    var x = matrix[12] || matrix[4];//translate x
+    return x;
+  }
 });
