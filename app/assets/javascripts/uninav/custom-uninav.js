b95095d86a9f58d9db74325635865eb31f662f44
Bryant <cbryant@chapman.edu>
2019-12-17 02:15:07 -0800

    adding logic for navigating up and down menu

--- app/assets/javascripts/uninav/custom-uninav.js
+++ app/assets/javascripts/uninav/custom-uninav.js
@@ -1,5 +1,9 @@
 $(document).ready( function() {
-  $('.uninav__hamburger-menu .off-canvas-nav .toggle').on('click', function() {
+  $('#uninav .off-canvas-nav .toggle').on('click', function() {
+    var $rootNav = $('.root-umbrella-nav');
     debugger
+    $(this).siblings('.drilldown-menu').show();
+    // $(this).siblings('.drilldown-menu').addClass('show');
+    $('.root-umbrella-nav').css({ transform: "translateX(-40rem)"});
   });
 });
