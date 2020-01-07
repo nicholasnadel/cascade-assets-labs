6c5d9639384d6ab86cb10a7683dfecd2a5b20aef
Nicholas Nadel <nicholasnadel@gmail.com>
2020-01-07 09:11:59 -0800

    testimonial widget accessibility - change keyup specificity

--- app/assets/javascripts/widgets/testimonial_widget.js
+++ app/assets/javascripts/widgets/testimonial_widget.js
@@ -15,18 +15,16 @@ $(function () {
     console.log('clicked')
     $(this).closest('.testimonial-widget').slick('slickNext');
   });
-
   // The plugin is not fully accessible (despite its claims) when there are links within a slide. Instead, it sets tabindex to -1 for slides not currently visible.  
   // On tab (within .testimonial-widget)
-  $('.testimonial-widget').on('focus', function(e){
-    $(window).keyup(function (e) {
-        var code = (e.keyCode ? e.keyCode : e.which);
-        if (code == 9) {
-          // Add tabindex 0 to a elements
-           $(".testimonial-widget a").each(function (i) { $(this).attr('tabindex', '0'); });
-        }
-    });
-});
-
-
+  $('.testimonial-widget').keyup(function (e) {
+    var code = (e.keyCode ? e.keyCode : e.which);
+    if (code == 9) {
+      // Add tabindex 0 to a elements
+      console.log('tab')
+      $(".testimonial-widget a").each(function (i) {
+        $(this).attr('tabindex', '0');
+      });
+    }
+  });
 });
