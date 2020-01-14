6867930a093f43271ba7226832d301aa393dc30e
Bryant <cbryant@chapman.edu>
2020-01-14 01:48:13 -0800

    set nav container height with js and locked scroll

--- app/assets/javascripts/uninav/left-nav.js
+++ app/assets/javascripts/uninav/left-nav.js
@@ -1,10 +1,115 @@
 $(document).ready(function() {
-  var drillDownSelectorsLeftNav = {
-    rootDrillDownNav: '#left-column-navigation .root-left-nav',
-    rootElement: '#left-column-navigation',
+  // var drillDownSelectorsLeftNav = {
+  //   rootDrillDownNav: '#left-column-navigation .root-left-nav',
+  //   rootElement: '#left-column-navigation',
+  // };
+
+  // var leftNavDrillDown = new DrillDownMenu(drillDownSelectorsLeftNav);
+  
+  // leftNavDrillDown.createClickHandlers();
+
+
+  /////////////////////////////
+  var $rootDrillDownNav   = $('#left-column-navigation .root-left-nav'),
+  $rootElement            = $('#left-column-navigation'),
+  // rootDrillDownNav   = selectors.rootDrillDownNav;
+  // rootElement        = selectors.rootElement;
+  resizeTimer        = null;
+
+
+  $rootElement.initialHeight = $rootDrillDownNav.css("height");
+  /**
+  * Run a callback on each item
+  * @param  {Function} callback The callback function to run
+  */
+ function drillMenuDown() {
+    var $menuToDrillDownTo  = $(this).siblings('.drilldown-menu'),
+    widthAmount             = $rootDrillDownNav[0].getBoundingClientRect().width,
+    ulCurrentPos            = getTranslateXVal($rootDrillDownNav),
+    translateXVal           = Math.floor(((ulCurrentPos - widthAmount) * 100) / 100 )  + "px";
+
+    $menuToDrillDownTo.show();
+    $rootDrillDownNav.css({ transform: "translateX(" + translateXVal + ")"  });
+    $rootElement.css({ height: $menuToDrillDownTo.height() });
+    // $rootElement.animate({ scrollTop: 0 }, 'slow');
+    
+    return;
+  }
+
+  /**
+  * Run a callback on each item
+  * @param  {Function} callback The callback function to run
+  */
+ function drillMenuUp() {
+    var widthAmount = $rootDrillDownNav[0].getBoundingClientRect().width,
+    ulCurrentPos    = getTranslateXVal($rootDrillDownNav),
+    translateXVal   = Math.floor(((ulCurrentPos + widthAmount) * 100) / 100 )  + "px"
+    $rootDrillDownNav.css({ transform: "translateX(" + translateXVal + ")"  });
+    $(this).parent().hide();
+
+    return;
+  }
+
+  function getTranslateXVal(selector) {
+    var transformMatrix = selector.css("-webkit-transform") ||
+                          selector.css("-moz-transform")    ||
+                          selector.css("-ms-transform")     ||
+                          selector.css("-o-transform")      ||
+                          selector.css("transform");
+    
+    transformMatrix = transformMatrix === "none" ? 0 : transformMatrix;
+
+    if (!isNaN(transformMatrix))
+      return 0;
+    
+    var matrix = transformMatrix.replace(/[^0-9\-.,]/g, '').split(',');
+    var x = matrix[12] || matrix[4];//translate x
+
+    return parseInt(x);
+  }
+
+  function moveOffCanvasToCurrentPathItem() {
+    var currentPath = $rootDrillDownNav.find('li .current');
+
+    if (currentPath.length) {
+      var $drillDownParents = currentPath.parents('ul.drilldown-menu'),
+      widthAmount           = Math.floor((($rootDrillDownNav[0].getBoundingClientRect().width) * 100) / 100 );
+
+      $drillDownParents.show();
+      $rootDrillDownNav.css({ transform: "translateX(-" + (widthAmount * $drillDownParents.length) + "px" });
+    }
+  }
+
+  function resizeRootDrillDown() {
+    var widthAmount       = $rootDrillDownNav[0].getBoundingClientRect().width,
+    ulCurrentPos          = getTranslateXVal($rootDrillDownNav),
+    $drillDownMenus       = $rootDrillDownNav.find('.drilldown-menu'),
+    $drillDownMenuVisible = $rootDrillDownNav.find('.drilldown-menu[style*="display: block"]')
+    $drillDownMenus.css({ left: widthAmount + "px" });
+    $rootDrillDownNav.css({ transform: "translateX(-" + (!ulCurrentPos ? ulCurrentPos :  widthAmount * $drillDownMenuVisible.length) + "px"});
+  }
+
+  function disableScroll() {
+    // debugger
+    $rootElement.scrollTop(0);
+    // console.log(this.pageYOffset);
+    // console.log(this.scrollTop);
+  }
+
+  $rootDrillDownNav.on('click', '.drill-down-parent', drillMenuDown);
+  
+  $rootDrillDownNav.on('click', '.toggle-drilldown', drillMenuDown);
+
+  $rootDrillDownNav.on('click', '.menu-back', drillMenuUp);
+
+  $rootElement.on('scroll', disableScroll);
+
+  var checkResizeRootDrillDown = function() {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(resizeRootDrillDown, 500);
   };
 
-  var leftNavDrillDown = new DrillDownMenu(drillDownSelectorsLeftNav);
-  
-  leftNavDrillDown.createClickHandlers();
+  $(window).resize(checkResizeRootDrillDown);
+
+  moveOffCanvasToCurrentPathItem();
 });
--- app/assets/javascripts/uninav/offcanvas-nav.js
+++ app/assets/javascripts/uninav/offcanvas-nav.js
@@ -28,7 +28,6 @@ $(document).ready( function() {
   // offCanvasMainDrillDown.createClickHandlers();
   // offCanvasUmbrellaDrillDown.createClickHandlers();
 
-
   $('#uninav .uninav__umbrella-nav-button-container button').on('click', function() {
     $offCanvasNavContainer.css({ transform: "translateX(" + menuVisibleXVal + "px)"});
   });
@@ -66,6 +65,14 @@ $(document).ready( function() {
     moveOffCanvasToRoot($(this));
   });
 
+  $rootDrillDownNavUmbrella.on('click', '.drill-down-parent', drillMenuDown);
+    
+  $rootDrillDownNavMain.on('click', '..drill-down-parent', drillMenuDown);
+
+  $rootDrillDownNavUmbrella.on('click', '.menu-back', drillMenuUp);
+
+  $rootDrillDownNavMain.on('click', '.menu-back', drillMenuUp);
+
   function changeContextualMenus($element) {
     var $otherContextualMenu = $element.parents('.off-canvas-menu').siblings('.off-canvas-menu'),
     $currentContextualMenu   = $element.parents('.off-canvas-menu');
@@ -96,6 +103,89 @@ $(document).ready( function() {
 
     return;
   }
+
+  function drillMenuDown() {
+    var $menuToDrillDownTo  = $(this).siblings('.drilldown-menu'),
+    ulCurrentPos            = getTranslateXVal($rootDrillDownNavMain),
+    umbrellaDrillDown       = $(this).parents('#off-canvas-umbrella').length,
+    translateXVal           = ulCurrentPos - menuWidth;
+    // debugger
+    if (umbrellaDrillDown) {
+      ulCurrentPos  = getTranslateXVal($rootDrillDownNavUmbrella),
+      translateXVal = ulCurrentPos - menuWidth;
+
+      $menuToDrillDownTo.show();
+      $rootDrillDownNavUmbrella.css({ transform: "translateX(" + translateXVal + "px)" });
+      $rootElementUmbrella.animate({ scrollTop: 0 }, 'slow');
+      return
+    }
+    
+    $menuToDrillDownTo.show();
+    $rootDrillDownNavMain.css({ transform: "translateX(" + translateXVal + "px)" });
+    $rootElementMain.animate({ scrollTop: 0 }, 'slow');
+    return;
+  }
+  
+  function drillMenuUp() {
+    var umbrellaDrillDown = $(this).parents('#off-canvas-umbrella').length,
+    ulCurrentPos  = getTranslateXVal($rootDrillDownNavMain),
+    translateXVal     = ulCurrentPos + menuWidth;
+
+    if (umbrellaDrillDown) {
+      ulCurrentPos  = getTranslateXVal($rootDrillDownNavUmbrella),
+      translateXVal = ulCurrentPos + menuWidth;
+
+      $rootDrillDownNavUmbrella.css({ transform: "translateX(" + translateXVal + "px)"  });
+      $(this).parent().hide();
+      return;
+    }
+  
+    $rootDrillDownNavMain.css({ transform: "translateX(" + translateXVal + "px)"  });
+    $(this).parent().hide();
+  
+    return;
+  }
+  
+  function getTranslateXVal(selector) {
+    var transformMatrix = selector.css("-webkit-transform") ||
+                          selector.css("-moz-transform")    ||
+                          selector.css("-ms-transform")     ||
+                          selector.css("-o-transform")      ||
+                          selector.css("transform");
+    
+    transformMatrix = transformMatrix === "none" ? 0 : transformMatrix;
+    if (!isNaN(transformMatrix))
+      return 0;
+    
+    var matrix = transformMatrix.replace(/[^0-9\-.,]/g, '').split(',');
+    var x = matrix[12] || matrix[4];//translate x
+  
+    return parseInt(x);
+  }
+  
+  function moveOffCanvasToCurrentPathItem() {
+    var currentPath = $offCanvasNavContainer.find('li .current');
+  
+    if (currentPath.length) {
+      
+      var $drillDownParents = currentPath.parents('ul.drilldown-menu'),
+      umbrellaDrillDown     = currentPath.parents('#off-canvas-umbrella').length;
+      $drillDownParents.show();
+  
+      if (umbrellaDrillDown) {
+        $rootUmbrellaDiv.show();
+        $rootMainDiv.hide();
+        $rootUmbrellaDiv.css({ transform: "translateX(-" + (menuWidth * $drillDownParents.length) + "px" });
+        $rootMainDiv.css({ transform: "translateX(-" + menuWidth + "px" });
+      }
+      $rootMainDiv.show();
+      $rootUmbrellaDiv.hide();
+      $rootMainDiv.css({ transform: "translateX(-" + ((menuWidth * 2) * $drillDownParents.length) + "px" });
+      $rootMainDiv.css({ transform: "translateX(-" + menuWidth + "px" });
+    }
+  }
+
+  moveOffCanvasToCurrentPathItem();
 });
 
 
@@ -103,10 +193,4 @@ $(document).ready( function() {
 ///////////////////////////////////////
 
 
-$rootDrillDownNavUmbrella.on('click', '.drill-down-parent', drillMenuDown);
-  
-$rootDrillDownNavMain.on('click', '.toggle-drilldown', drillMenuDown);
 
-$rootDrillDownNavUmbrella.on('click', '.menu-back', drillMenuDown);
-
-$rootDrillDownNavMain.on('click', '.menu-back', drillMenuDown);
