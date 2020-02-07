50c032cb6d23e618de682704f82b2fc9b692e684
nicholasnadel <nicholasnadel@gmail.com>
2020-02-06 22:00:36 -0800

    messing with handling errors; hide/show blocks

--- app/assets/javascripts/cascade/level/career_blocks.js
+++ app/assets/javascripts/cascade/level/career_blocks.js
@@ -1,89 +1,115 @@
 $(function() {
-  $.ajax({
-    type: "GET",
-    url:
-      "https://services.onetcenter.org/ws/online/occupations/17-2051.00?client=chapman",
-    data: { get_param: "value" },
-    async: true,
-    dataType: "json",
-    timeout: 10000,
-    error: function(jqxhr, statustext, err) {
-      if (jqxhr.status === 422) {
-        console.log(JSON.parse(jqxhr.responseText));
-      } else if (jqxhr.status) {
-        console.log({
-          error: "Call to " + url + " failed with error code " + jqxhr.status
-        });
-      } else if (statustext === "timeout") {
-        console.log({
-          error: "Call to " + url + " failed with no response from server"
-        });
-      } else if (err) {
-        console.log({
-          error: "Call to " + url + " failed with reason: " + err
-        });
-      } else {
-        console.log({
-          error: "Call to " + url + " failed with unknown reason"
-        });
-      }
-    },
-    success: function(data) {
-      $(".career-block-widget__title").html(
-        $("<div>", {
-          text: data.title
-        })
-      );
+  console.log("ready!");
+  $.each($(".career-block-widget"), function(ind) {
+    $(this).attr("id", "career-block-widget__" + parseInt(ind + 1));
+    var id = $(this).attr("id");
+    console.log("current id: " + id);
+    console.log("this " + id);
+    var careerCode = $(this)
+      .find(".career-block-widget__career-code")
+      .text()
+      .replace(/\s/g, "");
+    console.log("careerCode " + careerCode);
+    var URL =
+      "https://services.onetcenter.org/ws/online/occupations/" +
+      careerCode +
+      "?client=chapman";
+    console.log("url: " + URL);
+    /// ON SUCCESS CALLBACK
+    function onSuccess(data) {
+ 
+      //  $(this)
+      //   .find(".career-block-widget__text").addClass('fadeInUp');
+      $(this).removeClass('career-block-widget--hidden');
+      $(this).addClass('career-block-widget--reveal');
+      $(this)
+        .find(".career-block-widget__title")
+        .text(data.title).addClass('fadeInUp');
+      $(this)
+        .find(".career-block-widget__body")
+        .text(data.description).addClass('fadeInUp');
+      
 
-      $(".career-block-widget__body").html(
-        $("<div>", {
-          text: data.description
-        })
-      );
-
+             
+    }
+    var bindedOnSuccess = onSuccess.bind(this);
+    $.ajax({
+      type: "GET",
+      url: URL,
+      data: {
+        get_param: "value"
+      },
+      async: true,
+      dataType: "json",
+      timeout: 10000,
+      error: function(jqxhr, statustext, err) {
+        console.log(id + ' error');
+        if (jqxhr.status === 422) {
+          console.log(JSON.parse(jqxhr.responseText));
+        } else if (jqxhr.status) {
+          console.log({
+            error: "Call to " + url + " failed with error code " + jqxhr.status
+          });
+        } else if (statustext === "timeout") {
+          console.log({
+            error: "Call to " + url + " failed with no response from server"
+          });
+        } else if (err) {
+          console.log({
+            error: "Call to " + url + " failed with reason: " + err
+          });
+        } else {
+          console.log({
+            error: "Call to " + url + " failed with unknown reason"
+          });
+        }
+      },
+      success: bindedOnSuccess
+    }); // SALARY
+    // ON SUCCESS CALLBACK
+    function onSalarySuccess(data) {
+      var salary = formatMoney(data.salary.annual_median, 0);
+      $(".career-block-widget__salary").text("$" + salary + " Median Salary");
       console.log(data);
     }
+    var URLSalary =
+      "https://services.onetcenter.org/ws/mnm/careers/" +
+      careerCode +
+      "/job_outlook?client=chapman";
+    var salaryonSalarySuccess = onSalarySuccess.bind(this);
+    $.ajax({
+      type: "GET",
+      url: URLSalary,
+      data: {
+        get_param: "value"
+      },
+      async: true,
+      dataType: "json",
+      timeout: 10000,
+      error: function(jqxhr, statustext, err) {
+        if (jqxhr.status === 422) {
+          console.log(JSON.parse(jqxhr.responseText));
+        } else if (jqxhr.status) {
+          console.log({
+            error: "Call to " + url + " failed with error code " + jqxhr.status
+          });
+        } else if (statustext === "timeout") {
+          console.log({
+            error: "Call to " + url + " failed with no response from server"
+          });
+        } else if (err) {
+          console.log({
+            error: "Call to " + url + " failed with reason: " + err
+          });
+        } else {
+          console.log({
+            error: "Call to " + url + " failed with unknown reason"
+          });
+        }
+      },
+      success: salaryonSalarySuccess
+    });
   });
-
-  $.ajax({
-    type: "GET",
-    url:
-      "https://services.onetcenter.org/ws/mnm/careers/17-2051.00/job_outlook?client=chapman",
-    data: { get_param: "value" },
-    async: true,
-    dataType: "json",
-    timeout: 10000,
-    error: function(jqxhr, statustext, err) {
-      if (jqxhr.status === 422) {
-        console.log(JSON.parse(jqxhr.responseText));
-      } else if (jqxhr.status) {
-        console.log({
-          error: "Call to " + url + " failed with error code " + jqxhr.status
-        });
-      } else if (statustext === "timeout") {
-        console.log({
-          error: "Call to " + url + " failed with no response from server"
-        });
-      } else if (err) {
-        console.log({
-          error: "Call to " + url + " failed with reason: " + err
-        });
-      } else {
-        console.log({
-          error: "Call to " + url + " failed with unknown reason"
-        });
-      }
-    },
-    success: function(data) {
-      $(".career-block-widget__bold").html(
-        $("<span>", {
-          text: formatMoney(data.salary.annual_median)
-        })
-      );
-      console.log(data.salary.annual_median);
-    }
-  });
-
   function formatMoney(number, decPlaces, decSep, thouSep) {
     (decPlaces = isNaN((decPlaces = Math.abs(decPlaces))) ? 2 : decPlaces),
       (decSep = typeof decSep === "undefined" ? "." : decSep);
@@ -93,7 +119,6 @@ $(function() {
       parseInt((number = Math.abs(Number(number) || 0).toFixed(decPlaces)))
     );
     var j = (j = i.length) > 3 ? j % 3 : 0;
-
     return (
       sign +
       (j ? i.substr(0, j) + thouSep : "") +
@@ -106,9 +131,12 @@ $(function() {
         : "")
     );
   }
-
   document.getElementById("b").addEventListener("click", event => {
     document.getElementById("x").innerText =
       "Result was: " + formatMoney(document.getElementById("d").value);
   });
+
+  function addErrorClass() {
+            $(this).addClass('career-block-widget--ajax-error');
+  }
 });
