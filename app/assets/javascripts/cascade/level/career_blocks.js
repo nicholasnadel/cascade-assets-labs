9e4a96da0467b3feec3f44add9e3e369b6f5b7f6
nicholasnadel <nicholasnadel@gmail.com>
2020-02-07 21:00:30 -0800

    format money

--- app/assets/javascripts/cascade/level/career_blocks.js
+++ app/assets/javascripts/cascade/level/career_blocks.js
@@ -125,16 +125,38 @@ $(function() {
       if (typeof salary != "undefined") {
         $(this)
           .find(".career-block-widget__salary")
-          .text("$" + salary + " Median Salary");
+          .text("$" + formatMoney(salary, 0) + " Median Salary");
       } else {
         try {
           $(this)
             .find(".career-block-widget__salary")
-            .text("$" + salaryMedianOver + " Median Salary");
+            .text("$" + formatMoney(salaryMedianOver, 0) + " Median Salary");
         } catch {
           console.log($(this).prop(salary));
         }
       }
+    }
+
+    function formatMoney(number, decPlaces, decSep, thouSep) {
+      (decPlaces = isNaN((decPlaces = Math.abs(decPlaces))) ? 2 : decPlaces),
+        (decSep = typeof decSep === "undefined" ? "." : decSep);
+      thouSep = typeof thouSep === "undefined" ? "," : thouSep;
+      var sign = number < 0 ? "-" : "";
+      var i = String(
+        parseInt((number = Math.abs(Number(number) || 0).toFixed(decPlaces)))
+      );
+      var j = (j = i.length) > 3 ? j % 3 : 0;
+      return (
+        sign +
+        (j ? i.substr(0, j) + thouSep : "") +
+        i.substr(j).replace(/(\decSep{3})(?=\decSep)/g, "$1" + thouSep) +
+        (decPlaces
+          ? decSep +
+            Math.abs(number - i)
+              .toFixed(decPlaces)
+              .slice(2)
+          : "")
+      );
     }
   });
 });
