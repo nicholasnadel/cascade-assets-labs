268602b9605e376c9845ad7d5584f14e7131075e
nicholasnadel <nicholasnadel@gmail.com>
2020-02-19 19:12:33 -0800

    Add contact profile fallback for ie
    internet explorer

---
+++ app/assets/javascripts/cascade/level/contacat_profile_ie.js
@@ -0,0 +1,27 @@
+// object-fit fallback for ie internet explorer
+$(function() {
+  var ua = window.navigator.userAgent;
+  var msie = ua.indexOf("MSIE ");
+  if (
+    msie > 0 ||
+    (!!navigator.userAgent.match(/Trident.*rv\:11\./) &&
+      $(".contact-profile-widget__img").length)
+  ) {
+    $("img.contact-profile-widget__img").each(function() {
+      // Note: {.post-thumb img} is css selector of the image tag
+      var t = $(this),
+        s = "url(" + t.attr("src") + ")",
+        p = t.parent(),
+        d = $("<div class='ie__fallback--object-fit'></div>");
+      t.hide();
+      p.append(d);
+      d.css({
+        height: 150,
+        "background-size": "cover",
+        "background-repeat": "no-repeat",
+        "background-position": "center",
+        "background-image": s
+      });
+    });
+  }
+});
