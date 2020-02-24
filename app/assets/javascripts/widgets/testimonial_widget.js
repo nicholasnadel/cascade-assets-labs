ca5b8029ef7cd72be2ad3a9d7cb07d79cc9ec8b0
nicholasnadel <nicholasnadel@gmail.com>
2020-02-24 13:33:51 -0800

    Testimonial Widget - remove console.logs

--- app/assets/javascripts/widgets/testimonial_widget.js
+++ app/assets/javascripts/widgets/testimonial_widget.js
@@ -9,11 +9,9 @@ $(function () {
       variableWidth: false,
     });
     $('.testimonial-widget__button--left').click(function () {
-      console.log('clicked')
       $(this).closest('.testimonial-widget').slick('slickPrev');
     });
     $('.testimonial-widget__button--right').click(function () {
-      console.log('clicked')
       $(this).closest('.testimonial-widget').slick('slickNext');
     });
     // The plugin is not fully accessible (despite its claims) when there are links within a slide. Instead, it sets tabindex to -1 for slides not currently visible.  
@@ -22,7 +20,6 @@ $(function () {
       var code = (e.keyCode ? e.keyCode : e.which);
       if (code == 9) {
         // Add tabindex 0 to a elements
-        console.log('tab')
         $(".testimonial-widget a").each(function (i) {
           $(this).attr('tabindex', '0');
         });
