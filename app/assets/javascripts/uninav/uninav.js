6ef6e14a36d211bd18c353d2c2553c8202b486fc
nicholasnadel <nicholasnadel@gmail.com>
2020-03-16 14:25:59 -0700

    Revert "Uninav/Global-Nav Hover - save for another sprint"
    This reverts commit 1399ab499d182d49da685112f1ee33253676e22f.

--- app/assets/javascripts/uninav/uninav.js
+++ app/assets/javascripts/uninav/uninav.js
@@ -33,10 +33,8 @@ function toggleAriaExpandVal() {
       var menuItem = $(e.currentTarget);
       if (menuItem.attr("aria-expanded") === "true") {
         $(this).attr("aria-expanded", "false");
-        $(this).find('.uninav__dropdown--child').addClass('uninav__dropdown--child--collapse')
       } else {
         $(this).attr("aria-expanded", "true");
-        $(this).find('.uninav__dropdown--child').removeClass('uninav__dropdown--child--collapse')
       }
     }
   });
