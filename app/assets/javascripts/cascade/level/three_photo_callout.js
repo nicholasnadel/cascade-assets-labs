6afc3da79788c84101b1c016f3a2bc216ce86412
Nicholas <nicholasnadel@gmail.com>
2020-04-10 18:07:38 -0700

    Photo callout widget - add JS handle multiple widgets

--- app/assets/javascripts/cascade/level/three_photo_callout.js
+++ app/assets/javascripts/cascade/level/three_photo_callout.js
@@ -1,23 +1,35 @@
 $(document).ready(function () {
-    numberOfPhotos = $(".three-photo-callout-widget__container > a").size();
-    x = 6;
-    var count = 0;
-    $('.three-photo-callout-widget__container > a:lt(' + x + ')').show(0);
-    $('#three-photo-callout-widget__load-more').click(function () {
-        count += 1;
-        console.log('count ' + count);
 
-        if (count < 2) {
-            // condition ? value-if-true : value-if-false
-            x = (x + 3 <= numberOfPhotos) ? x + 3 : numberOfPhotos;
-            $('.three-photo-callout-widget__container > a:lt(' + x + ')').show(0);
-        } else if (count == 2) {
-            $('#three-photo-callout-widget__load-more').text('Load All')
-            x = (x + 3 <= numberOfPhotos) ? x + 3 : numberOfPhotos;
-            $('.three-photo-callout-widget__container > a:lt(' + x + ')').show(0);
-        } else if (count > 2) {
-            $('.three-photo-callout-widget__container > a').show(0);
-            $('#three-photo-callout-widget__load-more').fadeOut('slow');
-        }
+    $.each($('.three-photo-callout-widget__container'), function (ind) {
+        $(this).attr('id', 'three-photo-callout-widget__container__' + parseInt(ind + 1));
+        var currentWidgetContainer = $(this).closest('.three-photo-callout-widget__container').attr('id');
+        console.log(currentWidgetContainer)
+        var numberOfPhotos = $('#' + currentWidgetContainer).find("a").size();
+        console.log(numberOfPhotos);
+
+        var loadMoreButtonButton = ' + .three-photo-callout-widget__button';
+        var currentButton = '#' + currentWidgetContainer + loadMoreButtonButton;
+        console.log("'" + currentButton + "'");
+
+        x = 6;
+        var count = 0;
+        $('.three-photo-callout-widget__container > a:lt(' + x + ')').show(0);
+        $(currentButton).click(function () {
+            count += 1;
+            console.log('count ' + count);
+
+            if (count < 2) {
+                // condition ? value-if-true : value-if-false
+                x = (x + 3 <= numberOfPhotos) ? x + 3 : numberOfPhotos;
+                $('#' + currentWidgetContainer + ' > a:lt(' + x + ')').show(0);
+            } else if (count == 2) {
+                $(currentButton).text('Load All')
+                x = (x + 3 <= numberOfPhotos) ? x + 3 : numberOfPhotos;
+                $('#' + currentWidgetContainer + ' > a:lt(' + x + ')').show(0);
+            } else if (count > 2) {
+                $('#' + currentWidgetContainer + ' > a').show(0);
+                $(currentButton).fadeOut();
+            }
+        });
     });
 });
