96176c4e57582037ed16ec185bcff03f9c324c35
Nicholas <nicholasnadel@gmail.com>
2020-04-13 14:50:59 -0700

    Photo callout widget - refactor JS for multiple IDs

--- app/assets/javascripts/cascade/level/three_photo_callout.js
+++ app/assets/javascripts/cascade/level/three_photo_callout.js
@@ -1,35 +1,24 @@
 $(document).ready(function () {
-
     $.each($('.three-photo-callout-widget__container'), function (ind) {
         $(this).attr('id', 'three-photo-callout-widget__container__' + parseInt(ind + 1));
         var currentWidgetContainer = $(this).closest('.three-photo-callout-widget__container').attr('id');
         console.log(currentWidgetContainer)
-        var numberOfPhotos = $('#' + currentWidgetContainer).find("a").size();
-        console.log(numberOfPhotos);
-
+        var numberOfPhotos = $('#' + currentWidgetContainer + " > a").size()
+        console.log(currentWidgetContainer + numberOfPhotos);
         var loadMoreButtonButton = ' + .three-photo-callout-widget__button';
         var currentButton = '#' + currentWidgetContainer + loadMoreButtonButton;
         console.log("'" + currentButton + "'");
-
-        x = 6;
-        var count = 0;
-        $('.three-photo-callout-widget__container > a:lt(' + x + ')').show(0);
+        var numberOfPhotosToReveal = 3;
+        $('#' + currentWidgetContainer + ' > a:lt(' + 3 + ')').show();
         $(currentButton).click(function () {
-            count += 1;
-            console.log('count ' + count);
-
-            if (count < 2) {
-                // condition ? value-if-true : value-if-false
-                x = (x + 3 <= numberOfPhotos) ? x + 3 : numberOfPhotos;
-                $('#' + currentWidgetContainer + ' > a:lt(' + x + ')').show(0);
-            } else if (count == 2) {
-                $(currentButton).text('Load All')
-                x = (x + 3 <= numberOfPhotos) ? x + 3 : numberOfPhotos;
-                $('#' + currentWidgetContainer + ' > a:lt(' + x + ')').show(0);
-            } else if (count > 2) {
-                $('#' + currentWidgetContainer + ' > a').show(0);
-                $(currentButton).fadeOut();
-            }
+            var currentVisible = $('#' + currentWidgetContainer + ' a:visible').size()
+            numberOfPhotosToReveal = (numberOfPhotosToReveal + 3);
+            $('#' + currentWidgetContainer + ' > a:lt(' + numberOfPhotosToReveal + ')').show();
         });
     });
 });
+
+function resetCounter() {
+    var count = 0;
+    console.log('count ' + count);
+}
