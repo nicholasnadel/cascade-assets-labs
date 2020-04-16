8b69a04b1b65f777c08acc28ec4de76d61f6b1e9
Nicholas <nicholasnadel@gmail.com>
2020-04-16 09:05:09 -0700

    Photo callout widget - handle child divs (that are not wrapped links)

--- app/assets/javascripts/cascade/level/three_photo_callout.js
+++ app/assets/javascripts/cascade/level/three_photo_callout.js
@@ -1,48 +1,49 @@
 $(document).ready(function () {
-    $.each($('.three-photo-callout-widget__container'), function (ind, showLoadMoreButton) {
+    $.each($('.three-photo-callout-widget__container'), function (ind) {
         $(this).attr('id', 'three-photo-callout-widget__container__' + parseInt(ind + 1));
         var currentWidgetContainer = $(this).closest('.three-photo-callout-widget__container').attr('id');
         console.log(currentWidgetContainer)
-        var numberOfPhotos = $('#' + currentWidgetContainer + " > a").size()
+        var numberOfPhotos = $('#' + currentWidgetContainer + " img").size()
         console.log(currentWidgetContainer + numberOfPhotos);
         var loadMoreButtonButton = ' + .three-photo-callout-widget__button';
         var currentButton = '#' + currentWidgetContainer + loadMoreButtonButton;
         console.log("'" + currentButton + "'");
-        var numberOfPhotosToReveal = 3;
+        var numberOfPhotoLinksToReveal = 3;
+        var numberOfPhotoDivsToReveal = 3;
         $('#' + currentWidgetContainer + ' > a:lt(' + 3 + ')').show();
+        $('#' + currentWidgetContainer + ' > div:lt(' + 3 + ')').show();
         var buttonClickCounter = 0;
-
-
+        console.log('number photos: ' + numberOfPhotos)
         if (numberOfPhotos > 3) {
             $(currentButton).show();
+            console.log('revealing each load more button');
         }
-
         $(currentButton).click(function () {
             buttonClickCounter += 1;
-            var currentVisible = $('#' + currentWidgetContainer + ' a:visible').size()
-            numberOfPhotosToReveal = (numberOfPhotosToReveal + 3);
-            $('#' + currentWidgetContainer + ' > a:lt(' + numberOfPhotosToReveal + ')').show();
+            var currentVisible = $('#' + currentWidgetContainer + ' img:visible').size()
+            numberOfPhotoLinksToReveal = (numberOfPhotoLinksToReveal + 3);
+            $('#' + currentWidgetContainer + ' > a:lt(' + numberOfPhotoLinksToReveal + ')').show();
             if (buttonClickCounter < 2) {
-                $('#' + currentWidgetContainer + ' > a:lt(' + numberOfPhotosToReveal + ')').show();
+                $('#' + currentWidgetContainer + ' > a:lt(' + numberOfPhotoLinksToReveal + ')').show();
+                $('#' + currentWidgetContainer + ' > div:lt(' + numberOfPhotoLinksToReveal + ')').show();
             } else if (buttonClickCounter == 2 && numberOfPhotos > 6) {
                 $(currentButton).text('Load All')
-                $('#' + currentWidgetContainer + ' > a:lt(' + numberOfPhotosToReveal + ')').show();
+                $('#' + currentWidgetContainer + ' > a:lt(' + numberOfPhotoLinksToReveal + ')').show();
+                $('#' + currentWidgetContainer + ' > div:lt(' + numberOfPhotoLinksToReveal + ')').show();
             } else if (buttonClickCounter > 2) {
                 $('#' + currentWidgetContainer + ' > a').show(0);
+                $('#' + currentWidgetContainer + ' > div').show(0);
                 $(currentButton).text('All Photos Loaded')
                 // $(currentButton).fadeOut(5000);
             }
-
+            console.log('currentVisible: ' + currentWidgetContainer + ' ' + currentVisible)
             if (currentVisible == numberOfPhotos) {
                 $(currentButton).text('All Photos Loaded')
                 // $(currentButton).fadeOut(5000);
             }
-            debugger;
         });
     });
-
 });
-
 function resetbuttonClickCounterer() {
     var buttonClickCounter = 0;
     console.log('buttonClickCounter ' + buttonClickCounter);
