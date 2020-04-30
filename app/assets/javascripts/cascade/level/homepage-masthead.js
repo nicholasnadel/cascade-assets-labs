11a05b1eed6eb4b9e08b0f7f33d203cab8787533
Nicholas <nicholasnadel@gmail.com>
2020-04-30 15:17:47 -0700

    Homepage Masthead - add separate IE markup and styles

--- app/assets/javascripts/cascade/level/homepage-masthead.js
+++ app/assets/javascripts/cascade/level/homepage-masthead.js
@@ -8,7 +8,36 @@ $(function () {
         togglePlay();
     });
 
+
+    // object-fit fallback for ie internet explorer
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+    if (
+        msie > 0 ||
+        (!!navigator.userAgent.match(/Trident.*rv\:11\./) &&
+            $(".contact-profile-widget__img").length)
+    ) {
+        $(".homepage video").each(function () {
+            var t = $(this),
+                s = "url(" + t.attr("src") + ")",
+                p = t.parent(),
+                d = $("<div class='ie__fallback--object-fit'></div>");
+            t.hide();
+            p.append(d);
+            d.css({
+                height: 150,
+                "background-size": "cover",
+                "background-repeat": "no-repeat",
+                "background-position": "center",
+                "background-image": s
+            });
+        });
+    }
 });
+
+
+
+
 
 function togglePlay() {
     var vid = $("video#homepage-masthead__video");
