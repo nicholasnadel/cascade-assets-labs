99f4814095edab0e02933e74fcc19667e250e991
Nicholas <nicholasnadel@gmail.com>
2020-04-30 12:45:59 -0700

    Homepage Masthead - add max height to photo grid section

--- app/assets/javascripts/cascade/level/homepage-masthead.js
+++ app/assets/javascripts/cascade/level/homepage-masthead.js
@@ -1,40 +1,27 @@
 $(function () {
     var vid = $("video#homepage-masthead__video");
 
-    vid.controls = false;
+    $('.homepage video').removeAttr('controls');
 
-
-    // $("#homepage-masthead__pause-button").click(function () {
-    // if ($(vid).get(0).paused) {
-    //     $('video').trigger('play');
-    // } else {
-    //     $('video').trigger('pause');
-    // }
-    // });
 
     $('.homepage-masthead__toggle-play-button').on('click keydown', function (event) {
         togglePlay();
     });
-});
 
+});
 
 function togglePlay() {
     var vid = $("video#homepage-masthead__video");
     if ($(vid).get(0).paused) {
-        $(vid).trigger('play');
         vid.removeClass('homepage-masthead__play-video--paused')
-
         $('#homepage-masthead__play-button').hide();
         $('#homepage-masthead__pause-button').show();
-
-
+        $(vid).trigger('play');
     } else {
-        $(vid).trigger('pause');
         vid.addClass('homepage-masthead__video--paused')
         $('#homepage-masthead__pause-button').hide();
         $('#homepage-masthead__play-button').show();
-
-
+        $(vid).trigger('pause');
     }
 }
 // KEYS 🎹
