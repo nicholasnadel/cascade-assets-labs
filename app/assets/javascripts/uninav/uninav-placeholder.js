88dad9b80552c4b3de6f9beeffb3630bfd3d7da8
Nicholas <nicholasnadel@gmail.com>
2020-05-05 21:52:55 -0700

    Update static Uninav, Umbrella placeholders

--- app/assets/javascripts/uninav/uninav-placeholder.js
+++ app/assets/javascripts/uninav/uninav-placeholder.js
@@ -1,5 +1,8 @@
 $(function () {
 
-    $('.uninav-placeholder-modal').fadeOut('slow');
+
+    $("#uninav-static-close").click(function () {
+        $('.uninav-static-placeholder__notice').fadeOut(1200);
+    });
 
 });
