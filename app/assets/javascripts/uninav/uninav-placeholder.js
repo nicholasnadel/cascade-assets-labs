6b463414a623bce3164c5be0103738e7ae839837
Nicholas <nicholasnadel@gmail.com>
2020-05-07 15:23:21 -0700

    Static Uninav Placeholder - increase opacity, remove blur when click close button

--- app/assets/javascripts/uninav/uninav-placeholder.js
+++ app/assets/javascripts/uninav/uninav-placeholder.js
@@ -3,6 +3,7 @@ $(function () {
 
     $("#uninav-static-close").click(function () {
         $('.uninav-static-placeholder__notice').fadeOut(1200);
+        $('.uninav__logo, .uninav__global-nav, .uninav__utility-nav--wrapper, .uninav__cta').css('filter', 'blur(0)');
     });
 
 });
