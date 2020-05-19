7448532dd813b82c38e1b733910de495e4a531d2
Nicholas <nicholasnadel@gmail.com>
2020-05-19 08:17:46 -0700

    Add IE object-fit fallback for images

--- app/assets/javascripts/widgets/testimonial_widget.js
+++ app/assets/javascripts/widgets/testimonial_widget.js
@@ -33,5 +33,24 @@ $(function () {
     });
   };
   $('.testimonial-widget__cta:has(.button)').addClass('testimonial-widget__cta--button');
+
+  // IE Fallback for widget-slides img { object-fit: } CSS property 
+  if (document.documentMode || /Edge/.test(navigator.userAgent)) {
+    $("img.contact-profile-widget__img").each(function () {
+      var t = $(this),
+        s = "url(" + t.attr("src") + ")",
+        p = t.parent(),
+        d = $("<div class='ie__fallback--object-fit'></div>");
+      t.hide();
+      p.append(d);
+      d.css({
+        height: 150,
+        "background-size": "cover",
+        "background-repeat": "no-repeat",
+        "background-position": "center",
+        "background-image": s
+      });
+    });
+  }
 });
 
