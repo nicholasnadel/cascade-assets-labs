2c1cc391eb9d4ce8f5e23909185c5d99129ac8fb
Nicholas <nicholasnadel@gmail.com>
2020-05-20 08:12:14 -0700

    Next Steps Widget - spacing hotfix (Brendan)
    https://github.com/chapmanu/cascade-assets/commit/5ce07c3e580a0f81187bab7fc7523ee102fa6b35

--- app/controllers/content_types/modular_controller.rb
+++ app/controllers/content_types/modular_controller.rb
@@ -526,8 +526,8 @@ module ContentTypes
     def render_static_three_column_primary_content
       # This reproduces content from static sample version
       format("%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s",
-             render_static_partial("widgets/primary_content/wysiwyg_editor_3"),
-             render_static_partial("widgets/primary_content/testimonial_widget"),
+             render_static_partial("widgets/primary_content/next_steps_widget"),
+             render_static_partial("widgets/primary_content/text_with_cta"),
              render_static_partial("widgets/primary_content/collapsables_1"),
              render_static_partial("widgets/primary_content/collapsables_2"),
              render_static_partial("widgets/primary_content/funnel_1up_boxes_1"),
