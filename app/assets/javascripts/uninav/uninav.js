6abfd0e4cac6bf82891cc71bcad04bd34e43a9e5
Nicholas <nicholasnadel@gmail.com>
2020-06-05 10:14:01 -0700

    Accessibility/Uninav Search - add table role on window load

--- app/assets/javascripts/uninav/uninav.js
+++ app/assets/javascripts/uninav/uninav.js
@@ -117,12 +117,14 @@ function gs__customPlaceholder() {
 }
 // TODO: iOS style frosted/blurred background. CSS filter: blur(2px) performance is terrible
 $(window).load(function () {
-  $("#gsc-i-id1").on("input focus click", function () {
-    gs__blurBg();
-
-    // Google Search Table - add aria role
+  if ($('table.gstl_50').length) {
     $('table.gstl_50:not([role])').attr('role', 'presentation');
-  });
+    $("#gsc-i-id1").on("input focus click", function () {
+      gs__blurBg();
+      // Google Search Table - add aria role
+      $('table.gstl_50:not([role])').attr('role', 'presentation');
+    });
+  }
 });
 $(window).on("load resize", function (e) {
   gs__mobileReveal();
