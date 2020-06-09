f8e28e14b824983cbc65a1e98545455082b3ad10
Chris <chrs.bryant@gmail.com>
2020-06-09 16:45:55 -0700

    added accessibility listeners for tabbing to left nav

--- app/controllers/content_types/modular_controller.rb
+++ app/controllers/content_types/modular_controller.rb
@@ -515,11 +515,8 @@ module ContentTypes
 
     def render_static_two_column_left_column
       # This reproduces content from the static sample version
-      format("%s %s %s",
-             render_static_partial("widgets/left_column/left_nav"),
-             render_static_partial("widgets/left_column/callout_2"),
-             render_static_partial("widgets/left_column/contact_profile"),
-             render_static_partial("widgets/left_column/news_event_left_col"))
+      format("%s",
+             render_static_partial("widgets/left_column/left_nav"))
     end
 
     # rubocop:disable Metrics/AbcSize
