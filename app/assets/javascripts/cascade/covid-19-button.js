98ec85b4c73058e16d4a8d5548a5eaf87ec54f4e
Nicholas <nicholasnadel@gmail.com>
2020-06-23 20:47:11 -0700

    Covid Button tabbability followup (update a11y function)

---
+++ app/assets/javascripts/cascade/covid-19-button.js
@@ -0,0 +1,23 @@
+$(function () {
+    if ($('#covid19-callout-box').length > 0) {
+        $('#covid19-callout-box').on("keypress", function (e) {
+            if (a11yClick(event) === true) {
+                _toggleContactEl();
+            }
+        });
+
+        function a11yClick(event) {
+            if (event.type === 'click') {
+                return true;
+            } else if (event.type === 'keypress') {
+                var code = event.charCode || event.keyCode;
+                if ((code === 32) || (code === 13)) {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        }
+        // $("#covid19-callout-box").detach().appendTo('#main')
+    }
+});
