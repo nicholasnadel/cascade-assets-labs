e65a2fcfeedb35593a535be576de6bf9f4568175
Nicholas <nicholasnadel@gmail.com>
2020-06-24 20:29:43 -0700

    Add Global JS Functions file

---
+++ app/assets/javascripts/global_functions.js
@@ -0,0 +1,22 @@
+function accessibleClick(event) {
+    if (event.type === "click") {
+        return true;
+    } else if (event.type === "keypress") {
+        var code = event.charCode || event.keyCode;
+        if (code === 32 || code === 13) {
+            return true;
+        }
+    } else {
+        return false;
+    }
+}
+
+// global debug
+if (window.location.href.indexOf("???") > -1) {
+    // log and highlight the element with focus
+    document.addEventListener('focus', function () {
+        console.log('%c focused:  ', 'background: #222; color: yellow', document.activeElement);
+        $(document.activeElement).addClass('debug');
+    }, true);
+
+}
